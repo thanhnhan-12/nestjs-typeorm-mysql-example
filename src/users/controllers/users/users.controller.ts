9652def84ad82549ad1c23dfb92421b5c96bdf74
thanhnhan-12 <nguyenthanhnhan12701@gmail.com>
2024-05-30 15:36:59 +0700

    create nestjs example

---
+++ src/users/controllers/users/users.controller.ts
@@ -0,0 +1,10 @@
+import { Controller, Get, Post } from '@nestjs/common';
+
+@Controller('users')
+export class UsersController {
+  @Get()
+  getUsers() {}
+
+  @Post()
+  createUser() {}
+}
---
+++ src/users/service/users/users.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from 'src/typeorm/entities/User';
+import { Repository } from 'typeorm';
+
+@Injectable()
+export class UsersService {
+  constructor(
+    @InjectRepository(User) private userRepository: Repository<User>,
+  ) {}
+
+  findUsers() {}
+
+  createUsers() {}
+}
